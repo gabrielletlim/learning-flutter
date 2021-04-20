368ecad2cb745e9d598602eeb9e9ed99901dc705
gabrielletlim <gabrielle.lim.1@etsmtl.net>
2021-04-20 15:50:48 -0400

    I Am Rich - Part 2

--- complete-flutter-dev-bootcamp/i_am_rich/lib/main.dart
+++ complete-flutter-dev-bootcamp/i_am_rich/lib/main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(child: Text("Hello World")),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1611627125959-712f6a5feef0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'),
+        ),
+      ),
+    ),
   ));
 }
