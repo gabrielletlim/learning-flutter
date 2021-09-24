c6d16009238d3795326c756438acd7fca4587bc5
gabrielletlim <gabrielle.lim.1@etsmtl.net>
2021-09-24 12:57:33 -0400

    Finish dicee app

--- complete-flutter-dev-bootcamp/dicee/lib/main.dart
+++ complete-flutter-dev-bootcamp/dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,20 +16,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+  int generateNumber() => Random().nextInt(6)+1;
+
+  void setNewValues() {
+    setState(() {
+      leftDiceNumber = generateNumber();
+      rightDiceNumber = generateNumber();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          )),
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                onPressed: () => setNewValues(),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                onPressed: () => setNewValues(),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
         ],
       ),
     );
